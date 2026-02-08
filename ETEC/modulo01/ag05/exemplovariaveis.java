b26c4e330bb98b10959853d2ad72c0b2119709c5
G13Dantas <gdantasestudos01@gmail.com>
2026-02-08 09:33:42 -0300

    Treino

---
+++ ETEC/modulo01/ag05/exemplovariaveis.java
@@ -0,0 +1,31 @@
+public class exemplovariaveis {
+    public static void main (String args []) {
+        //Declaração de variáveis
+        //Inteiros
+        byte numeroByte = 127;
+        short numeroShort = 32767;
+        int numeroInt = 214748647;
+        Long numeroLong = 9223372036854775807;
+        //Reais
+        float numetoFloat = 10^38;
+        double numeroDouble = 10^308;
+        //Caractere
+        char caractereChar = 'A';
+        String textoString = "Texto Exemplo";
+        //Boolenano
+        boolean VerdadeiroBoolean = true;
+        boolena FalsoBooblean = false;
+
+        //Exibição de valores
+        System.out.println("ValorByte: " + numeroByte);
+        System.out.println("ValorShort: " + numeroShort);
+        System.out.println("ValorInt: " + numeroInt);
+        System.out.println("ValorLong: " + numeroLong);
+        System.out.println("ValorFloat: " + numetoFloat);
+        System.out.println("ValorDouble: " + numeroDouble);
+        System.out.println("ValorChar: " + caractereChar);
+        System.out.println("ValorString: " + textoString);
+        System.out.println("ValorBoolean Verdadeiro: " + VerdadeiroBoolean);
+        System.out.println("ValorBoolean Falso: " + FalsoBooblean);
+    }
+}
