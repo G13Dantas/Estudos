3d329a94fc042c903628a6ee710496d011fd1b37
G13Dantas <gdantasestudos01@gmail.com>
2026-02-14 21:20:19 -0300

    me

--- ETEC/modulo01/ag05/SomaValores.java
+++ ETEC/modulo01/ag05/SomaValores.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
-public class SomaValores {}{
+public class SomaValores {
     public static void main(String args []) {
 
         int numero1, numero2 , soma;
 
         Scanner leitor = new Scanner(System.in);
+
+        //descrição programa
+        System.out.println("Programa para somar dois valores");
+        
+        //entrada de dados
+        System.out.println("Digite o primeiro valor: ");
+        numero1 = leitor.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        numero2 = leitor.nextInt();
+
+        //processamento de dados
+        soma = numero1 + numero2;
+
+        //saida de dados
+        System.out.println("A soma dos valores é: " + soma);
+        
+        // fecha o scanner para evitar vazamento de recurso
+        leitor.close();
+
     }
     
 }
--- ETEC/modulo01/ag05/exemplovariaveis.java
+++ ETEC/modulo01/ag05/exemplovariaveis.java
@@ -4,28 +4,28 @@ public class exemplovariaveis {
         //Inteiros
         byte numeroByte = 127;
         short numeroShort = 32767;
-        int numeroInt = 214748647;
-        Long numeroLong = 9223372036854775807;
+        int numeroInt = 2147483647;
+        long numeroLong = 9223372036854775807L;
         //Reais
-        float numetoFloat = 10^38;
-        double numeroDouble = 10^308;
+        float numeroFloat = 1e38f;
+        double numeroDouble = 1e308;
         //Caractere
         char caractereChar = 'A';
         String textoString = "Texto Exemplo";
-        //Boolenano
-        boolean VerdadeiroBoolean = true;
-        boolena FalsoBooblean = false;
+        //Booleano
+        boolean verdadeiroBoolean = true;
+        boolean falsoBoolean = false;
 
         //Exibição de valores
         System.out.println("ValorByte: " + numeroByte);
         System.out.println("ValorShort: " + numeroShort);
         System.out.println("ValorInt: " + numeroInt);
         System.out.println("ValorLong: " + numeroLong);
-        System.out.println("ValorFloat: " + numetoFloat);
+        System.out.println("ValorFloat: " + numeroFloat);
         System.out.println("ValorDouble: " + numeroDouble);
         System.out.println("ValorChar: " + caractereChar);
         System.out.println("ValorString: " + textoString);
-        System.out.println("ValorBoolean Verdadeiro: " + VerdadeiroBoolean);
-        System.out.println("ValorBoolean Falso: " + FalsoBooblean);
+        System.out.println("ValorBoolean Verdadeiro: " + verdadeiroBoolean);
+        System.out.println("ValorBoolean Falso: " + falsoBoolean);
     }
 }
